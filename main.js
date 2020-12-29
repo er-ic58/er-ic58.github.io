7d0c798f9fb0a86ba8e157073aacb732404677a9
er-ic58 <spbot50@gmail.com>
2020-12-29 11:35:16 -0700

    added stats

--- main.js
+++ main.js
@@ -7,10 +7,37 @@ dropdown.addEventListener('click', function(event) {
 }));
 */
 
+function arrayToTable(tableData) {
+    var table = $('<table class="js-sort-table table is-striped is-hoverable is-fullwidth"></table>');
+    var thead = table.append($('<thead></thead>'));
+    var tbody = table.append($('<tbody></tbody>'));
+    $(tableData).each(function (i, rowData) {
+        var row = $('<tr></tr>');
+        var dtype = i==0 ? 'th' : 'td';
+        var app = i==0 ? thead : tbody;
+        $(rowData).each(function (j, cellData) {
+            row.append($('<'+dtype+'>'+cellData+'</'+dtype+'>'));
+        });
+        app.append(row);
+    });
+    return table;
+}
+
 setupPage = function(){
-  var hashParams = window.location.hash.substr(1).split('&'); // substr(1) to remove the `#`
+  var hashParams = window.location.search.substr(1).split('&'); // substr(1) to remove the `#`
+  if (hashParams[0] == "") return;
   for(var i = 0; i < hashParams.length; i++){
       var p = hashParams[i].split('=');
-      document.getElementById(p[0]).value = decodeURIComponent(p[1]);;
+      document.querySelector('[name="'+p[0]+'"]').value = decodeURIComponent(p[1]);
   }
+
+  var season = document.querySelector('[name="season"]').value;
+
+  $.ajax({
+        type: "GET",
+        url: "stats/"+season+".csv",
+        success: function (data) {
+            $('#tablelocation').append(arrayToTable(Papa.parse(data).data));
+        }
+    });
 }
